fc143f2d1e6595cd316fc4586e039fbb1d029d13
danieljml <danielgarrido425@gmail.com>
2023-11-27 11:21:59 -0400

    Updates

---
+++ assets/Resume-nnnM_SHH.js
@@ -0,0 +1,7 @@
+import{r,_ as s,j as e}from"./index-602ZtZvM.js";const t=r.lazy(()=>s(()=>import("./PdfViewer-ZV-Oceo7.js"),__vite__mapDeps([0,1,2,3]))),o=()=>e.jsx(r.Suspense,{fallback:"",children:e.jsx(t,{})});export{o as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/PdfViewer-ZV-Oceo7.js","assets/index-602ZtZvM.js","assets/index-9g1TtRmO.css","assets/PdfViewer-uDaNkwkE.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
